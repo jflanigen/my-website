0a9d5521fd82ba2faa70718c574fe48462de55b6
jflanigen <joeyflan@bu.edu>
2024-08-25 21:14:58 +1000

    trying to fix compatibility with routing/React/github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import HomePage from './components/mainpages/HomePage';
 import AboutPage from './components/mainpages/AboutPage';
 import ExperiencePage from './components/mainpages/ExperiencePage';
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-500 cursor-default">
       <div className="bg-fixed bg-gradient-to-r from-slate-900 from-5% via-slate-800 via-50% to-slate-900 to-95% h-5/6 w-full">
-        <Router>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
@@ -20,7 +20,7 @@ function App() {
             <Route path="/project-compiler" element={<CompilerPage />} />
             <Route path="/project-keyracer" element={<KeyRacerPage />} />
           </Routes>
-        </Router>
+        </HashRouter>
       </div>
     </div>
   );
